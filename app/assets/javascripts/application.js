e8de394f18eea0410c67ab602ead79df2e98e66c
Leonardo Scorza <leonardoscorzas88@gmail.com>
2017-12-07 20:29:34 -0200

    include page selector js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require materialize-sprockets
+//= jquery-readyselector
 //= require_tree .
---
+++ app/assets/javascripts/jquery-readyselector.js
@@ -0,0 +1,16 @@
+(function ($) {
+  var ready = $.fn.ready;
+  $.fn.ready = function (fn) {
+    if (this.context === undefined) {
+      ready(fn);
+    } else if (this.selector) {
+      ready($.proxy(function(){
+        $(this.selector, this.context).each(fn);
+      }, this));
+    } else {
+      ready($.proxy(function(){
+        $(this).each(fn);
+      }, this));
+    }
+  }
+})(jQuery);
